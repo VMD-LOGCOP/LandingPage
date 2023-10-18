65d00839f63848bb70133fbfd94b620aa6cf797c
janctr <janiverson.eligio.ctr@us.navy.mil>
2023-10-18 12:53:23 -1000

    Format utils.js

--- utils.js
+++ utils.js
@@ -1,12 +1,12 @@
 define(['qlik'], function (qlik) {
-    const appId = qlik.currApp().id
+    const appId = qlik.currApp().id;
 
     const sheets = qlik.navigation.sheets.map(
         ({ qInfo: { qId }, qMeta: { title } }) => ({ label: title, value: qId })
-    )
+    );
 
     function exportDataToClipboard(data) {
-        const { pageSettings, menuItems } = data
+        const { pageSettings, menuItems } = data;
 
         // Export object to clipboard
         // {
@@ -16,104 +16,104 @@ define(['qlik'], function (qlik) {
         const json = JSON.stringify({
             pageSettings,
             menuItems,
-        })
+        });
         navigator.clipboard.writeText(json).then(() => {
-            alert('Successfully copied to clipboard')
-        })
+            alert('Successfully copied to clipboard');
+        });
     }
 
     function importDataFromClipboard({ qInfo: { qId } }) {
-        const backendApi = window.backendApi[qId]
+        const backendApi = window.backendApi[qId];
 
         navigator.clipboard.readText().then((json) => {
-            const data = JSON.parse(json)
+            const data = JSON.parse(json);
 
-            const { pageSettings, menuItems } = data
+            const { pageSettings, menuItems } = data;
 
             backendApi.getProperties().then(function (reply) {
-                if (pageSettings) reply.pageSettings = pageSettings
-                if (menuItems) reply.menuItems = menuItems
+                if (pageSettings) reply.pageSettings = pageSettings;
+                if (menuItems) reply.menuItems = menuItems;
 
-                console.log('setting properties: ', reply)
-                backendApi.setProperties(reply)
-            })
-        })
+                console.log('setting properties: ', reply);
+                backendApi.setProperties(reply);
+            });
+        });
     }
 
     function getObjectId(layout) {
         // You can use this to target elements specific to an object
-        return layout.qInfo.qId
+        return layout.qInfo.qId;
     }
 
     function getObjectTitleId(layout) {
         // `header#${layout.qInfo.qId}_title` - targets the area at the top that leave unwanted whitespace;
-        return `${layout.qInfo.qId}_title`
+        return `${layout.qInfo.qId}_title`;
     }
 
     function getObjectContentId(layout) {
         // `#${layout.qInfo.qId}_content` - targets the body of the object
-        return `${layout.qInfo.qId}_content`
+        return `${layout.qInfo.qId}_content`;
     }
 
     function isWebLink(menuItem) {
-        return menuItem.linkType === 'web-link'
+        return menuItem.linkType === 'web-link';
     }
 
     function isSheetLink(menuItem) {
-        return menuItem.linkType === 'sheet-link'
+        return menuItem.linkType === 'sheet-link';
     }
 
     function isNotLink(menuItem) {
-        return menuItem.linkType === 'no-link'
+        return menuItem.linkType === 'no-link';
     }
 
     function getSheetUrl({ isSipr, sheetId }) {
-        const appId = qlik.currApp().id
+        const appId = qlik.currApp().id;
 
         return `https://qlik.advana.data${
             isSipr ? '.smil' : ''
-        }.mil/sense/app/${appId}/sheet/${sheetId}`
+        }.mil/sense/app/${appId}/sheet/${sheetId}`;
     }
 
     function getHref({ menuItem, isSipr }) {
         switch (menuItem.linkType) {
             case 'web-link':
-                return menuItem.href
+                return menuItem.href;
             case 'sheet-link':
-                return getSheetUrl({ isSipr, sheetId: menuItem.sheetId })
+                return getSheetUrl({ isSipr, sheetId: menuItem.sheetId });
             default:
-                return 'invalid link'
+                return 'invalid link';
         }
     }
 
     function getBackgroundImageUrl({ menuItem }) {
-        const appId = qlik.currApp().id
-        const filename = menuItem.coverImageUrl
+        const appId = qlik.currApp().id;
+        const filename = menuItem.coverImageUrl;
 
-        return `url(/appcontent/${appId}/${filename})`
+        return `url(/appcontent/${appId}/${filename})`;
     }
 
     function makeClassFromTitle(menuItem) {
-        if (!menuItem.cardTitle && !menuItem.cardSubtitle) return false
+        if (!menuItem.cardTitle && !menuItem.cardSubtitle) return false;
 
-        let className = menuItem.cardTitle || menuItem.cardSubtitle
+        let className = menuItem.cardTitle || menuItem.cardSubtitle;
 
         return className
             .split(' ')
             .map((s) => s.trim())
-            .join('-')
+            .join('-');
     }
 
     function navigateToSheet(sheetId) {
-        qlik.navigation.gotoSheet(sheetId)
+        qlik.navigation.gotoSheet(sheetId);
     }
 
     function navigateToUrl(url) {
-        window.location = url
+        window.location = url;
     }
 
     function navigateToUrlInNewTab(url) {
-        window.open(url, '_blank')
+        window.open(url, '_blank');
     }
 
     return {
@@ -134,5 +134,5 @@ define(['qlik'], function (qlik) {
         navigateToSheet,
         navigateToUrl,
         navigateToUrlInNewTab,
-    }
-})
+    };
+});
